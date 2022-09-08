f267892494cd9a3920cb74ba6dbff9f02230f860
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-08 16:10:31 +0200

    Insérer une entité en base de données 2/4

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Entity\Task;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class TaskController extends AbstractController
 {
     #[Route('/create-project')]
-    public function createProject(): Response
+    public function createProject(ManagerRegistry $doctrine): Response
     {
-        return new Response();
+        $entityManager = $doctrine->getManager();
+
+        $project = new Project();
+        $project->setTitle('Titre du projet');
+        $project->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua');
+
+        $entityManager->persist($project);
+        $entityManager->flush();
+
+        return new Response(sprintf('Projet %s créé', $project->getTitle()));
+    }
+
+    #[Route('/create-task')]
+    public function createTask(ManagerRegistry $doctrine): Response
+    {
+        $entityManger  = $doctrine->getManager();
+
+        $task = new Task();
+        $task->setTitle('Titre de la tâche');
+        $task->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua');
+
+        $entityManger->persist($task);
+        $entityManger->flush();
+
+        return new Response(sprintf('Tâche %s créée', $task->getTitle()));
     }
 }
